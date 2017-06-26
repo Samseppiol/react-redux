13557b57931b2836fa32eb8beb56817f36fdbf0c
Samseppiol <j_hage@live.com>
2017-06-26 14:27:51 +1000

    Importing react to index.js

--- section_1/src/index.js
+++ section_1/src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 // Want to create a new component
 //  it should produce some html 
 
@@ -6,3 +8,8 @@ const App = function() {
 }
 
 // Take the component's HTML and display it on the page
+
+
+// Wont work, react is undefined
+// Need to import React at the top of the file.
+React.render(App);
