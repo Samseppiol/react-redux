b4c78db31b18eb9c77c94247e3679c5204284470
Samseppiol <j_hage@live.com>
2017-06-27 08:24:12 +1000

    refactored functional app component to a class component an set the state

--- section_2/README.md
+++ section_2/README.md
@@ -2,3 +2,7 @@
 
 ### Downwards data flow
 > The most parent component in the application should be responsible for fetching data. In our instance, index.js
+
+### Syntactic Sugar
+> When the key and the variable name are the same when setting the state, because of ES6 you only have to type one of them.
+> E.g this.setState({videos: videos})<br> can be changed to this.setState({videos})
--- section_2/src/index.js
+++ section_2/src/index.js
@@ -1,5 +1,5 @@
 // Importing react as a javascript module
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -7,21 +7,36 @@ import YTSearch from 'youtube-api-search';
 // Importing components 
 import SearchBar from './components/search_bar';
 
+// Declaring youtube api key
 const API_KEY = 'AIzaSyAEHNh8n3YmrJuepd4_bhzzTEgDfE6s_tk'
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-})
+
+
 // Want to create a new component
 //  it should produce some html 
 
-const App = () => {
-  return (
-  <div> 
-    <SearchBar />
-  </div>
-  );
-};
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      // this.setState({videos: videos})
+      this.setState({ videos })
+    });
+  }
+
+
+  render () {
+    return (
+    <div> 
+      <SearchBar />
+    </div>
+    );
+  };
+}
 
 // Take the component's HTML and display it on the page
 
