6699d063abb2632fc08ffe3cc4deebaede5d316a
Samseppiol <j_hage@live.com>
2017-06-28 08:13:38 +1000

    added markup to video list item, refactored code with some es6 syntax

--- section_2/src/components/video_list_item.js
+++ section_2/src/components/video_list_item.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-const VideoListItem = (props) => {
-  return<li>Video</li>
+
+// By passing video in curly braces we don't need to write props then const video = props.video
+const VideoListItem = ({video}) => {
+  const imageUrl = video.snippet.thumbnails.default.url;
+  
+  // const video = props.video;
+  return (
+    <li className='list-group-item'>
+      <div className='video-list media'> 
+
+        <div className='media-left'> 
+          <img className='media-object' src={imageUrl} />
+        </div> 
+
+        <div className='media-body'> 
+          <div className='media-heading'>
+            {video.snippet.title}
+          </div> 
+        </div> 
+
+      </div>  
+
+    </li>
+  );
 };
 
 export default VideoListItem;
